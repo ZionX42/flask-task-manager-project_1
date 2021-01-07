7e2558e6c45c4dfed6e4cd64a59f533b62e55130
Rick N <rickmnassar@gmail.com>
2021-01-07 11:35:22 +0000

    Add Task Collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
+  
 /*
     jQuery for MaterializeCSS initialization
 */
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
 });
-
-
